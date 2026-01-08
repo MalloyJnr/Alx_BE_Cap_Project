1cbdbb7f1557f7a11abcdd4af94102bf1f38ce01
Malloy-Lucio <mltechguides@gmail.com>
2026-01-08 18:41:08 +0000

    created by field added to the model

--- churchfund/contributions/models.py
+++ churchfund/contributions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from members.models import Member
 
 class Contribution(models.Model):
@@ -8,10 +9,23 @@ class Contribution(models.Model):
         ("offering", "Offering"),
     ]
 
-    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="contributions")
+    member = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name="contributions"
+    )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     contribution_type = models.CharField(max_length=20, choices=CONTRIBUTION_TYPES)
-    date = models.DateField()
+
+    date = models.DateField(auto_now_add=True)
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
     remarks = models.TextField(blank=True)
 
     def __str__(self):
