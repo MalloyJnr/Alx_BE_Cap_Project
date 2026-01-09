40f5270b769423f3dab453370d5e818bef8710db
Malloy-Lucio <mltechguides@gmail.com>
2026-01-09 10:40:03 +0000

    total contribution by type added

--- churchfund/contributions/views.py
+++ churchfund/contributions/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
 from .models import Contribution
 from .serializers import ContributionSerializer
+from django.db.models import Sum
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class ContributionViewSet(viewsets.ModelViewSet):
     queryset = Contribution.objects.all().order_by("-date")
@@ -9,3 +12,13 @@ class ContributionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+        
+    @action(detail=False, methods=['get'], url_path='summary-by-type')   
+    def summary_by_type(self, request):
+        data = (
+        Contribution.objects
+        .values('contribution_type')
+        .annotate(total_amount=Sum('amount'))
+        )
+        results = {item['contribution_type']: item['total_amount'] for item in data}   
+        return Response(results)
